8fa9b76b3ca64ab77ad3ef6b02e60ce6c98ab8ae
Grace Gjerde <grace.gjerde@reuben.ox.ac.uk>
2025-10-20 11:18:58 +0100

    edited L39 space

--- ci_course/functionality.py
+++ ci_course/functionality.py
@@ -35,7 +35,6 @@ def minimum(*args):
     if not any([isinstance(arg, numbers.Real) for arg in args]):
         return
 
-
     the_min = float("inf")
     for arg in args:
         if isinstance(arg, numbers.Real):
